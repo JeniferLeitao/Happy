09e196cc5336df3437d50851b0c4c0037f7b49e1
Jenifer Leitao <jenifer.leitao@gmail.com>
2020-10-20 16:05:22 -0700

    02

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,30 +1,38 @@
 import express from 'express';
+import {getRepository} from 'typeorm';
+import Orphanage from './models/Orphanage';
+
+import './database/connection'
 
 const app = express();
 
 app.use(express.json());
 
- /**
-  * Rota = conjunto
-  * Recurso = usiário
-  * 
-  * Métodos HTTP = GET, POST, PUT, DELETE
-  * 
-  * GET = Buscar uma informação
-  * POST = Criando uma informação
-  * PUT = Editando uma informação
-  * DELETE = Deletando uma informação
-  * 
-  * Parâmetros
-  * Query Params: http://localhost:3333/users?search=jenifer
-  * Route Params: http://localhost:3333/users/1 (Identificar um recurso)
-  * Body: http://localhost:3333/users
-  */
 
-app.get('/users/:id', (request, response) => {
-    console.log(request.query);
-    console.log(request.params);
-    console.log(request.body);
+app.post('/orphanages', async (request, response) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    });
+
+    await orphanagesRepository.save(orphanage)
 
     return response.json({message: "Hello World"});
 });
